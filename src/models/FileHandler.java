9111a025eef3518be1f7c1977888f66b3498be9c
csengekulker <k71608883429@ktch.hu>
2022-12-19 09:41:54 +0100

    testek hozzáadva

--- src/models/FileHandler.java
+++ src/models/FileHandler.java
@@ -1,8 +1,33 @@
 package models;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class FileHandler {
-    
-    public void readFile() {
-        
+    public String fileName;
+
+    public FileHandler() {
+        this.fileName = "szotar.txt";
+    }
+
+    public void readFile(){
+        try{
+            tryReadFile();
+        }
+        catch(FileNotFoundException e){
+            System.out.println("Hiba ! A file nem található");
+        }
+    }
+
+    public void tryReadFile() throws FileNotFoundException{
+        File file = new File(this.fileName);
+        Scanner scanner = new Scanner(file);
+        while(scanner.hasNextLine()){
+            String line = scanner.nextLine();
+            System.out.println(line);
+        }
+
+        scanner.close();
     }
 }
--- test/SzotarTest.java
+++ test/SzotarTest.java
@@ -1,5 +1,7 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 
@@ -13,6 +15,7 @@ public class SzotarTest {
     @BeforeEach
     void setMethodList() {
         this.methodNameList = new ArrayList<>();
+        //lekérjük a metodusokat
         Method[] methods = FileHandler.class.getDeclaredMethods();
         for(Method method : methods) {
             this.methodNameList.add(method.getName());
@@ -22,6 +25,32 @@ public class SzotarTest {
     void checkReadFileMethod() {
         boolean hasReadFileMethod = this.methodNameList.contains("readFile");
         assertTrue(hasReadFileMethod);
+    }
+
+    ArrayList<String> fieldNameList;
+    @BeforeEach
+    void setFieldList() {
+        this.fieldNameList = new ArrayList<>();
+        //lekérjük a metodusokat
+        Field[] fields = FileHandler.class.getDeclaredFields();
+        for(Field field : fields) {
+            this.fieldNameList.add(field.getName());
+        }
+    }
+
+    //write test to this
+    @Test
+    void checkFileName(){
+        boolean hasFileName = this.fieldNameList.contains("fileName");
+        assertTrue(hasFileName, "Hiba! A fileName mező nem létezik");
+    }
+    @Test
+    void checkFileNameContent(){
+        //megvizsgáljuk a file nevét
+        FileHandler fileHandler = new FileHandler();
+        boolean isGoodName = fileHandler.fileName.equals("szotar.txt");
+
+        assertTrue(isGoodName, "Hiba! A fájlnév nem szotar");
     }
 
 }
